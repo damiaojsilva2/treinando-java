d35b3cf4c67f87722f0b447aa51a97557be15125
damiaojsilva2 <damiaojsilva2@gmail.com>
2023-10-04 11:30:36 -0300

    Mais uma etapa de estudos java

---
+++ src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for(int carnerinhos = 1; carnerinhos <=20; carnerinhos ++ ){
+            System.out.println("Contando carnerinhos " + carnerinhos);
+        }
+        
+        System.out.println("Joazinho dormiu");
+
+    }
+}
